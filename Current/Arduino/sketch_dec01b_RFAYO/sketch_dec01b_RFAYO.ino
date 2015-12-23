3543b59d05750a9747f2566b0dd8df66a9283d7e
Phionx <shanjha.usa@gmail.com>
2015-12-23 18:50:41 -0500

    2015end

---
+++ Current/Arduino/sketch_dec01b_RFAYO/sketch_dec01b_RFAYO.ino
@@ -0,0 +1,42 @@
+#include <RFduinoGZLL.h> // include rfduino library
+device_t role = DEVICE4; // set Device name... DEVICE2 to DEVICE7 / HOST
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+void setup() {
+  // initialize serial:
+  Serial.begin(115200);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+  RFduinoGZLL.txPowerLevel = 0;
+  RFduinoGZLL.begin(role); // begin BLE communications
+}
+void loop() {
+  Serial.println("A");
+  delay(25);
+  serialEvent(); //call the function
+  // print the string when a newline arrives:
+  char mydata[25]; // declare mydata array+
+  if (stringComplete) {
+    Serial.println(inputString);
+    inputString.toCharArray(mydata, 25);
+    RFduinoGZLL.sendToHost(mydata, 25); // send buffer to host other rfduino
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+    else     inputString += inChar;
+  }
+}
+void RFduinoGZLL_onReceive(device_t device, int rssi, char *data, int len) // this function receives BLE communications
+{
+
+}
---
+++ Current/Arduino/sketch_dec01d_hostayo/sketch_dec01d_hostayo.ino
@@ -0,0 +1,55 @@
+/*
+This sketch demonstrates how to send data from a Device
+to a Host in a Gazell network.
+
+The host and upto 3 devices should have the RGB shield
+attached.  When Button A on a Device is pressed, the
+associated led on the Host will toggle.  Device1 is
+associated with the Red led, Device2 with the Green led
+and Device3 with the Blue led.
+
+The Green led on the Device will blink to indicate
+that an acknowledgement from the Host was received.
+*/
+
+#include <RFduinoGZLL.h>
+
+device_t role = HOST;
+
+// pin for the Green Led
+String sstr1, sstr2, sstr3;
+
+void setup()
+{
+Serial.begin(115200);
+  // start the GZLL stack  
+  RFduinoGZLL.begin(role);
+}
+
+void loop()
+{
+  Serial.flush();
+
+  if(sstr1!="")  Serial.println(sstr1);
+  if(sstr2!="")  Serial.println(sstr2);
+  if(sstr3!="")  Serial.println(sstr3);
+  sstr1="";
+  sstr2="";
+  sstr3="";
+}
+
+void RFduinoGZLL_onReceive(device_t device, int rssi, char *data, int len)
+{
+  if(device== DEVICE2)
+  {
+  sstr1=data;
+  }
+  else if(device== DEVICE3)
+  {
+    sstr2=data;
+  }
+  else if(device== DEVICE4)
+  {
+    sstr3=data;
+  }
+}
